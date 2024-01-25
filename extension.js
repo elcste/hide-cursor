3f7735f10a02a1caf7bde54b5d06b23a5aaf9020
Alexander Browne <elcste@users.noreply.github.com>
2024-01-25 08:59:51 -0600

    extension.js:37 fix indentation

--- extension.js
+++ extension.js
@@ -34,7 +34,7 @@ export default class HideCursor extends Extension {
             let tracker = Meta.CursorTracker.get_for_display(global.display);
             const seat = Clutter.get_default_backend().get_default_seat();
 
-		    if (!seat.is_unfocus_inhibited())
+            if (!seat.is_unfocus_inhibited())
                 seat.inhibit_unfocus();
             tracker.set_pointer_visible(false);
 
